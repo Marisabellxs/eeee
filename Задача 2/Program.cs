256d690a41c2d2e6ae7ff8440c6e1767fe6c2106
Marisabell <mary_tusa@mail.ru>
2022-12-03 20:48:40 +0400

    Решение 2 задачи

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Марисабель!");
+﻿Console.WriteLine("Введите первое число");
+int firstnumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondnumber = int.Parse(Console.ReadLine());
+
+if (firstnumber>secondnumber)
+{
+    Console.WriteLine("Первое число больше второго");
+}
+else
+{
+    Console.WriteLine("Второе число больше первого");
+}
