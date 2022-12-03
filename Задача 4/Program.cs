d646c3469f6c1ddd531a088d4cfdfa09921dc183
Marisabell <mary_tusa@mail.ru>
2022-12-03 21:45:38 +0400

    Решение 4 задачи

--- Задача 4/Program.cs
+++ Задача 4/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int firstnumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int secondnumber = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int thirdnumber = int.Parse(Console.ReadLine());
+
+if (firstnumber>=secondnumber & secondnumber>=thirdnumber)
+{
+    Console.WriteLine("Первое число максимальное");
+}
+else
+{
+    if (secondnumber>=thirdnumber)
+    {
+        Console.WriteLine("Второе число максимальное");
+    }
+    else
+    {
+        Console.WriteLine("Третье число максимальное");
+    }
+}
