153e4e922881b596b396533fe9d9f508e90305b9
Marisabell <mary_tusa@mail.ru>
2022-12-04 00:39:57 +0400

    Корректировка Write и WriteLine

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -2,12 +2,17 @@
 int firstnumber = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число");
 int secondnumber = int.Parse(Console.ReadLine());
+int max = 0;
 
 if (firstnumber>secondnumber)
 {
-    Console.WriteLine("Первое число больше второго");
+    max = firstnumber;
+    Console.Write("Max = ");
+    Console.WriteLine(max);
 }
 else
 {
-    Console.WriteLine("Второе число больше первого");
+    max = secondnumber;
+    Console.Write("Max = ");
+    Console.WriteLine(max);
 }
